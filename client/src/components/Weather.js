2e13aad63f74feb569e5466330f1242849e37970
Deep Patel <73080455+dvkpatel11@users.noreply.github.com>
2023-03-22 14:08:45 -0400

    Update Weather.js

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -1,21 +1,10 @@
 import React from 'react';
 
-function Weather({ weather }) {
-  const temperature = Math.round(weather.main.temp);
-  const feelsLike = Math.round(weather.main.feels_like);
-  const condition = weather.weather[0].description;
-  const icon = weather.weather[0].icon;
-
+function Weather({ weatherData }) {
   return (
-    <>
-      <img
-        src={`https://openweathermap.org/img/wn/${icon}.png`}
-        alt="weather icon"
-      />
-      <p>Temperature: {temperature}°C</p>
-      <p>Feels like: {feelsLike}°C</p>
-      <p>Weather condition: {condition}</p>
-    </>
+    <div className="weather">
+      <h2 className="description">{weatherData.description}</h2>
+    </div>
   );
 }
 
