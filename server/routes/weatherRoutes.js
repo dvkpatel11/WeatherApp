c701c700d042f65b932a2ef469b9287eeee44dad
Deep Patel <73080455+dvkpatel11@users.noreply.github.com>
2023-03-22 16:15:45 -0400

    Update weatherRoutes.js

--- server/routes/weatherRoutes.js
+++ server/routes/weatherRoutes.js
@@ -1,35 +1,8 @@
 const express = require('express');
-const axios = require('axios');
+const weatherController = require('../controllers/weatherController');
+
 const router = express.Router();
 
-require('dotenv').config(); // load environment variables from .env file
-
-router.get('/:city', async (req, res) => {
-  try {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&units=metric&appid=`);
-
-const express = require('express');
-const axios = require('axios');
-const router = express.Router();
-
-require('dotenv').config(); // load environment variables from .env file
-
-router.get('/api/weather/:city', async (req, res) => {
-  try {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&units=metric&appid=1f51cc58da604a917c31d97eee52c545`);
-
-    const weatherData = {
-      temp: response.data.main.temp,
-      feelsLike: response.data.main.feels_like,
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon
-    };
-
-    res.json(weatherData);
-  } catch (error) {
-    console.error(error);
-    res.status(404).json({ error: 'Invalid city name.' });
-  }
-});
+router.get('/:city', weatherController.getWeatherByCity);
 
 module.exports = router;
