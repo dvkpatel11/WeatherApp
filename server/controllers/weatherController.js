6ff79c9a129110f344f5a5bc775f927d249432bd
Deep Patel <73080455+dvkpatel11@users.noreply.github.com>
2023-03-22 16:17:08 -0400

    Update weatherController.js

--- server/controllers/weatherController.js
+++ server/controllers/weatherController.js
@@ -1,8 +1,18 @@
-const express = require('express');
-const weatherController = require('../controllers/weatherController');
+const axios = require('axios');
 
-const router = express.Router();
+const API_KEY = 1f51cc58da604a917c31d97eee52c545; // Replace with your OpenWeatherMap API key
 
-router.get('/:city', weatherController.getWeatherByCity);
-
-module.exports = router;
+exports.getWeatherByCity = async (req, res) => {
+  try {
+    const { city } = req.params;
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    if (error.response) {
+      res.status(error.response.status).json({ message: error.response.statusText });
+    } else {
+      res.status(500).json({ message: 'An error occurred while fetching weather data.' });
+    }
+  }
+};
