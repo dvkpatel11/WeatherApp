053106e5b736c3379d10e2272e8fd6e905608550
Deep Patel <73080455+dvkpatel11@users.noreply.github.com>
2023-03-22 16:04:56 -0400

    Update Weather.js

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import '../styles/WeatherCard.css';
 
-function Weather({ weatherData }) {
+function WeatherCard(props) {
+  const { name, weather, main } = props.data;
+
   return (
-    <div className="weather">
-      <h2 className="description">{weatherData.description}</h2>
+    <div className="WeatherCard">
+      <h2>{name}</h2>
+      <div className="WeatherCard-condition">
+        <img src={`https://openweathermap.org/img/w/${weather[0].icon}.png`} alt={weather[0].description} />
+        <p>{weather[0].description}</p>
+      </div>
+      <div className="WeatherCard-temperature">
+        <p>{Math.round(main.temp)}&deg;C</p>
+        <p>Feels like {Math.round(main.feels_like)}&deg;C</p>
+      </div>
+      <div className="WeatherCard-details">
+        <p>Humidity: {main.humidity}%</p>
+        <p>Pressure: {main.pressure} hPa</p>
+        <p>Wind: {Math.round(props.data.wind.speed * 3.6)} km/h</p>
+      </div>
     </div>
   );
 }
 
-export default Weather;
+export default WeatherCard;
