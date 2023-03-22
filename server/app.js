63a165e375721b1abc6cce809a284c256bbc3352
Deep Patel <73080455+dvkpatel11@users.noreply.github.com>
2023-03-22 16:29:26 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const weatherRoutes = require('./routes/weatherRoutes');
 
 const app = express();
 
+// Enable CORS
 app.use(cors());
-app.use(express.json());
+
+// Serve static files from the client/build directory
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+// Handle API routes
 app.use('/api/weather', weatherRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// Handle client-side routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
