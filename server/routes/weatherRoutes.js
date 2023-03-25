2dfda6ee95b4078c8ae13e2e0cc02eded3b080e9
Deep Patel <73080455+dvkpatel11@users.noreply.github.com>
2023-03-24 21:13:36 -0400

    Delete server directory

--- server/routes/weatherRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const weatherController = require('../controllers/weatherController');
-
-const router = express.Router();
-
-router.get('/:city', weatherController.getWeatherByCity);
-
-module.exports = router;
